b9b0eb5016099387862707b74f83b2885f8901b4
BoriskataMBT <bobialeksandrov5@gmail.com>
2024-11-01 16:36:12 +0200

    Updated sln to ex 1

--- Methods.05/ex.01/Program.cs
+++ Methods.05/ex.01/Program.cs
@@ -1,12 +1,62 @@
 ﻿
-namespace ex.01
+using System.Diagnostics.CodeAnalysis;
+
+namespace ex01
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string input = Console.ReadLine();
-            char input[0]
+            int input = int.Parse(Console.ReadLine());
+            for (int i = 0; i < input; i++)
+            {
+                bool isDigitOdd = IsAnyDigitOdd(i);
+                bool divideBy8 = IsDivideabelBy8(i);
+                if (isDigitOdd && divideBy8)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        public static bool IsAnyDigitOdd(int i)
+        {
+            string temp = i.ToString();
+            int tempInt;
+
+            for (int j = 0; j < temp.Length; j++)
+            {
+                tempInt = int.Parse(temp[j].ToString());
+
+                if (tempInt % 2 == 1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool IsDivideabelBy8(int i)
+        {
+            int sum = 0;
+            string temp = i.ToString();
+            int tempInt;
+
+            for (int j = 0; j < temp.Length; j++)
+            {
+                tempInt = int.Parse(temp[j].ToString());
+
+                sum += tempInt;
+            }
+
+
+            if (sum % 8 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
